d81a283f62ad51604e4d119c7436da5568ae4c65
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-25 11:46:12 +0530

    Created out first group and moving to a course

--- expo-routing/app/_layout.tsx
+++ expo-routing/app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router'
 const RootLayout = () => {
   return (
     <Stack>
+      <Stack.Screen name='(tab)' options={{headerShown: false}}/> 
       <Stack.Screen name='index' options={{headerShown: false}}/>
       <Stack.Screen name='about'/>
     </Stack>
