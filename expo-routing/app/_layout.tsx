90c373a8d2b1584adea9cbb20166d8eb957265dc
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-25 11:20:40 +0530

    Usage of _layout.tsx

--- expo-routing/app/_layout.tsx
+++ expo-routing/app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name='index' options={{headerShown: false}}/>
+      <Stack.Screen name='about'/>
     </Stack>
-  );
+  )
 }
+
+export default RootLayout
