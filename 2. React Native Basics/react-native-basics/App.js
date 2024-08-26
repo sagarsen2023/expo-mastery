79b917f819d9d32be848303d9bdc265d626e6118
Sagar Sen <sagarsen.dev@gmail.com>
2024-08-26 09:35:56 +0530

    Styled the text in the map function

--- 2. React Native Basics/react-native-basics/App.js
+++ 2. React Native Basics/react-native-basics/App.js
@@ -1,17 +1,44 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 // We mainly work with core components like View, Text, Button, Image, etc.
-export default function App() {
-  return <View style={styles.appContainer}>
-    <View style={styles.inputContainer}>
-     <TextInput style={styles.textInput} placeholder="Your goal"/>
-     <Button title="Add Goal"/>
-    </View>
+// Here styles are not being cascaded like in web development. We have to apply styles to each component separately.
 
-    <View style={styles.goalsContainer}>
-      <Text>List of Goals</Text>
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function goatInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your goal"
+          onChangeText={goatInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        {goals.map((goal, index) => (
+          <View key={goal} style={styles.goalItems}>
+            <Text style={{color: "white "}}>
+              {goal}
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
-  </View>;
+  );
 }
 
 const styles = StyleSheet.create({
@@ -20,23 +47,29 @@ const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 16,
   },
-  inputContainer : {
+  inputContainer: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 2,
-    borderBottomColor: "#cccccc"
+    borderBottomColor: "#cccccc",
   },
-  textInput : {
+  textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%" ,
+    width: "70%",
     marginRight: 8,
-    padding: 8
+    padding: 8,
   },
-  goalsContainer : {
+  goalsContainer: {
     flex: 4,
-  }
+  },
+  goalItems: {
+    margin: 8,
+    padding: 8,
+    backgroundColor: "#5e0acc",
+    borderRadius: 6,
+  },
 });
